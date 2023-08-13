e77e67ad0ff0159872b38fe7dfe56cc9069f1f0a
César Tzoc <csartalva01@gmail.com>
2023-08-13 15:05:19 -0600

    added backend base

---
+++ schedule-system-rest/src/main/java/com/cesar31/schedulesystemrest/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.cesar31.schedulesystemrest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class HelloApplication extends Application {
+
+}
