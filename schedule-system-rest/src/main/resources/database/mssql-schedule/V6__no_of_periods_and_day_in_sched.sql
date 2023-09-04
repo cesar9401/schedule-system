6b53117321d3da93407c006a22bf88d1460bb66c
César Tzoc <csartalva01@gmail.com>
2023-09-03 22:34:51 -0600

    added new cols to build the schedule
    added number of periods in ac_cy_subject
    added day in ac_cy_sched_subj

---
+++ schedule-system-rest/src/main/resources/database/mssql-schedule/V6__no_of_periods_and_day_in_sched.sql
@@ -0,0 +1,4 @@
+-- number of periods
+ALTER TABLE dbo.ac_cy_subject ADD number_of_periods BIGINT CONSTRAINT ac_cy_subject_number_of_periods_default DEFAULT 1 NOT NULL;
+-- day of the week for the schedule
+ALTER TABLE dbo.ac_cy_sched_subj ADD cat_day BIGINT CONSTRAINT ac_cy_subject_number_of_periods_default DEFAULT 1 NOT NULL;
