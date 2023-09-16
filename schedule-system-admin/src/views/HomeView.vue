331a56d08097e3ec9fd706090675701c4f87fb51
César Tzoc <csartalva01@gmail.com>
2023-09-16 16:30:30 -0600

    added base vue project

---
+++ schedule-system-admin/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+
+console.log(import.meta.env.VITE_SCHEDULE_SYSTEM_REST_URL);
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
